cc1287d3ac336271d4cf05933b5c5800b45c1dfa
Ann999-coder <annvismaya16@gmail.com>
2021-06-23 21:26:30 +0530

    changes in the index.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Title,Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'project-management';
+
+  constructor(private titleService: Title, private meta: Meta)
+  {
+    titleService.setTitle('NOSCE Project Management System');
+    meta.addTag({name: 'viewport', content: 'width=device-width, initial-scale=1, shrink-to-fit=no'},true);
+    meta.addTag({name: 'keywords', content: 'HR, employee management'},true);
+    meta.addTag({name: 'author', content: 'NOSCE Technosolutions'},true);
+  }
 }
---
+++ src/app/project.service.ts
@@ -0,0 +1,55 @@
+import { HttpClient , HttpErrorResponse, HttpHeaders} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/internal/Observable';
+import { catchError, tap } from 'rxjs/operators';
+import { Project } from './project';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProjectService {
+
+  project:Project[] | undefined;
+
+
+  private apiServer = "http://localhost:8090/project";
+
+  constructor(private httpClient: HttpClient) {}
+    create(p:Project) {
+     
+      const headers = { 'content-type': 'application/json'}  
+       const body=JSON.stringify(p);
+       console.log('project'+body)
+      return this.httpClient.post(this.apiServer + '/save', body);
+      
+    }
+   
+    getprojectByID(id : number){
+
+      return this.httpClient.get<Project>(this.apiServer +id)
+    }
+
+    delete(id :number){
+   
+      console.log("id"+id)
+      return this.httpClient.delete<Project>(this.apiServer + '/delete/' + id,)
+     
+    }
+
+    //update(id ,data){
+
+      //return this.httpClient.put(this.apiServer + id+ '/update' +,data)
+    //}
+
+    getProjectAll(): Observable<Project[]> {
+      return this.httpClient.get<Project[]>(this.apiServer +"/all" )
+      
+    }
+    errorHandler(errorHandler: any): import("rxjs").OperatorFunction<Project[], any> {
+      throw new Error('Method not implemented.');
+    }
+
+  
+
+}
